4821a2050208dec3cc30ffdfc26bc2fcbda23bda
brunoRobs <git.brunoRobs.excal@gmail.com>
2025-03-28 14:53:50 -0300

    Show sheet refactor and download sheet function

--- src/main/java/com/roleplay_network/rpn/controllers/SheetController.java
+++ src/main/java/com/roleplay_network/rpn/controllers/SheetController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.roleplay_network.rpn.services.SheetService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,13 +20,25 @@ public class SheetController {
   @Autowired
   SheetService sheetService;
 
-  @GetMapping("/{name}")
-  public ResponseEntity<?> getSheet(@PathVariable String name) {
+  @GetMapping("/show/{name}")
+  public ResponseEntity<?> showSheet(@PathVariable String name) {
     byte[] sheet = sheetService.find(name);
     if (sheet != null) {
       HttpHeaders header = new HttpHeaders();
-      header.add(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=sample.pdf");
-      header.add(HttpHeaders.CONTENT_TYPE, "application/pdf");
+      header.setContentType(MediaType.APPLICATION_PDF);
+      header.setContentDisposition(ContentDisposition.builder("inline").filename(name).build());
+      return new ResponseEntity<>(sheet, header, HttpStatus.OK);
+    }
+    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+  }
+
+  @GetMapping("/download/{name}")
+  public ResponseEntity<?> downloadSheet(@PathVariable String name) {
+    byte[] sheet = sheetService.find(name);
+    if (sheet != null) {
+      HttpHeaders header = new HttpHeaders();
+      header.setContentType(MediaType.APPLICATION_PDF);
+      header.setContentDisposition(ContentDisposition.builder("attachment").filename(name).build());
       return new ResponseEntity<>(sheet, header, HttpStatus.OK);
     }
     return new ResponseEntity<>(HttpStatus.NOT_FOUND);
