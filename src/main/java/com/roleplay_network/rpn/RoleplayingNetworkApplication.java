818fdd5dd77e6128db8773123bfd5f102801e77d
brunoRobs <git.brunoRobs.excal@gmail.com>
2025-03-27 23:04:51 -0300

    first commit

---
+++ src/main/java/com/roleplay_network/rpn/RoleplayingNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.roleplay_network.rpn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleplayingNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleplayingNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roleplay_network/rpn/RoleplayingNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roleplay_network.rpn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleplayingNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
